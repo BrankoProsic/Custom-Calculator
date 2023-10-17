d779809afa757653ac9940f7f3a1b061663ab1f3
Mladen Radosovic <mladen.radosovic@gig-ext.com>
2023-10-17 13:01:56 +0200

    Kad hvatas izuzetke prvo hvatas specificne pa tek na kraju generalni

--- Custom Calculator/SaveToSQLServerRepository.cs
+++ Custom Calculator/SaveToSQLServerRepository.cs
@@ -1,81 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace Custom_Calculator
 {
     public class SaveToSQLServerRepository : IRepository
     {
-        // ovde ide impelemntacija koju si gurnuo u formu, onako grubo :D
         public string connStr = ConfigurationManager.ConnectionStrings["CustomCalculatorString"].ConnectionString;
 
-        //public void SaveHistory(string text) // prouci sql injection i videces zasto ovo nije dobra praksa, POPORAVI!!
-        //{
-        //    SqlConnection conn = new SqlConnection(connStr);
-        //    conn.Open();
-
-        //    string sqlTxt = "INSERT INTO History(Logs) VALUES ('" + text + "')";
-        //    SqlCommand cmdInsert = new SqlCommand(sqlTxt, conn);
-
-        //    cmdInsert.ExecuteNonQuery();
-        //    conn.Close();
-        //}
-
-        //public void SaveHistory(string text) //PARAMETARISED QUERIE!
-        //{
-        //    SqlConnection conn = new SqlConnection(connStr);
-        //    conn.Open();
-
-        //    string sqlTxt = "INSERT INTO History(Logs) VALUES (@Logs)";
-        //    SqlCommand cmdInsert = new SqlCommand(sqlTxt, conn);
-        //    cmdInsert.Parameters.AddWithValue("@Logs", text);
-
-        //    cmdInsert.ExecuteNonQuery();
-        //    conn.Close();
-        //    //Proverio i radi
-        //}
-
-        //public void SaveHistory(string text) //STORED PROCEDURE
-        //{
-        //    SqlConnection conn = new SqlConnection(connStr);
-        //    conn.Open();
-
-        //    string sqlTxt = "calculatorLogs";
-        //    SqlCommand cmdInsert = new SqlCommand(sqlTxt, conn);
-        //    cmdInsert.CommandType = System.Data.CommandType.StoredProcedure;
-        //    cmdInsert.Parameters.AddWithValue("@Logs", text);
-
-        //    cmdInsert.ExecuteNonQuery();
-        //    conn.Close();
-        //}
-        public void SaveHistory(string text) //PARAMETARISED QUERIE!
+        public void SaveHistory(string text) 
         {
             SqlConnection conn = new SqlConnection(connStr);
             try
             {
                 conn.Open();
-                DateTime time = DateTime.Now;
                 string sqlTxt = "INSERT INTO DateHistory(Date, Logs) VALUES (@Date, @Logs)";
                 SqlCommand cmdInsert = new SqlCommand(sqlTxt, conn);
-                cmdInsert.Parameters.AddWithValue("@Date", time);
+                cmdInsert.Parameters.AddWithValue("@Date", DateTime.Now);
                 cmdInsert.Parameters.AddWithValue("@Logs", text);
 
                 cmdInsert.ExecuteNonQuery();
+            }
+            #region catch
+            catch (InvalidCastException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (ObjectDisposedException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (InvalidOperationException ex)
+            {
+                MessageBox.Show(ex.Message);
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
             }
+            #endregion catch
             finally
             {
                 conn.Close();
             }
-            //Proverio i radi
         }
     }
 }
