123940545d2915f1d67c6b12569feeb7dc60dfdf
Mladen Radosovic <mladen.radosovic@gig-ext.com>
2023-10-18 13:03:18 +0200

    uklanjanje suvisnog koda i komentara

--- Custom Calculator/SaveToTextFileRepository.cs
+++ Custom Calculator/SaveToTextFileRepository.cs
@@ -1,21 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
+﻿
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Custom_Calculator
 {
     public class SaveToTextFileRepository : IRepository
     {
         
-        public void SaveHistory(string text) // ova metoda je beskorisna trenutno, popravi je
-            // pokusaj da shvatis kako da upotrebis parametar, ona mora da dodbije neki tekst da bi ga upisala!!!
+        public void SaveHistory(string text) 
         {
             string filePath =
                 @"C:\Users\brank\Desktop\PROJECTS\APLIKACIJE\CALCULATORS\Calculator WF\Custom Calculator\history.txt";
-            //string history =
             File.AppendAllText(filePath, text);
 
         }
