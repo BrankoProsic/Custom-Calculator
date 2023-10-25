03d4dfdd3ffc46003f11e39e77a2d92128d7e0c1
Mladen Radosovic <mladen.radosovic@gig-ext.com>
2023-10-25 14:37:34 +0200

    Sredjivanje logike cuvanja fajla

--- Custom Calculator/SaveToTextFileRepository.cs
+++ Custom Calculator/SaveToTextFileRepository.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Configuration;
 using System.IO;
 using System.Windows.Forms;
@@ -7,73 +8,58 @@ namespace Custom_Calculator
 {
     public class SaveToTextFileRepository : IRepository
     {
-        public void SaveHistory(string text) // zavrsi ovaj deo ako nemas nesto drugo zanimljivo trenutno
+        public void SaveHistory(string text) // zavrsi ovu metodu
         {
             string putanja = ConfigurationManager.AppSettings.Get("PutanjaZaCuvanjeFajla");
-            string ime = ConfigurationManager.AppSettings.Get("ImeFajla");
-            string tacnaLokacija = $"{putanja}{ime}";
 
             SaveFileDialog saveText = new SaveFileDialog();
             saveText.Title = "Save file as";
             saveText.Filter = "Text Files (*.txt)|*.txt|All files (*.*)|*.*";
-            saveText.FileName = ime;
-            saveText.FilterIndex = 1;
-            saveText.RestoreDirectory = true;
-            saveText.OverwritePrompt = false;
-            
-            if (!File.Exists(saveText.FileName))
+
+            try
             {
-                if (saveText.ShowDialog() == DialogResult.OK)
+                if (File.Exists(putanja))
                 {
-                    TextWriter writer = new StreamWriter(saveText.FileName);
-                    writer.Write(text);
-                    writer.Close();
+                    File.AppendAllText(putanja, text);
+                }
+
+                else
+                {
+                    if (saveText.ShowDialog() == DialogResult.OK)
+                    {
+                        TextWriter writer = new StreamWriter(saveText.FileName);
+                        writer.Write(text);
+                        writer.Close();
+                        AddOrUpdateAppSettings("PutanjaZaCuvanjeFajla", saveText.FileName);
+                    }
                 }
             }
-            else 
-            {
-                if (File.Exists(tacnaLokacija))
-                {
-                    File.AppendAllLines(tacnaLokacija, text);
-                }
-                //{
-                //    StreamWriter sw = new StreamWriter(putanja);
-                //    File.AppendAllLines(putanja, text);
-                //    //File.AppendAllLines(putanja, text);
-                //    //saveText.FileName = putanja;
-                //}
-            }
+            catch { }
 
-            //string filePath =
-            //    @"C:\Users\brank\Desktop\PROJECTS\APLIKACIJE\CALCULATORS\Calculator WF\Custom Calculator\history.txt";
-
-            //if (!File.Exists(filePath))
-            //{
-
-            //    File.Create(filePath);
-            //    TextWriter calculatorHistory = new StreamWriter(filePath);
-            //    File.WriteAllText(filePath, text);
-            //    calculatorHistory.Close();
-            //}
-            //else if (File.Exists(filePath))
-            //{
-            //    File.AppendAllText(filePath, text);
-            //}
         }
 
-        //private void CheckFile()
-        //{
-        //    string path = $"{directory}{displayHistory}";
+        public static void AddOrUpdateAppSettings(string key, string value)
+        {
+            try
+            {
+                var configFile = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+                var settings = configFile.AppSettings.Settings;
+                if (settings[key] == null)
+                {
+                    settings.Add(key, value);
+                }
+                else
+                {
+                    settings[key].Value = value;
+                }
+                configFile.Save(ConfigurationSaveMode.Modified);
+                ConfigurationManager.RefreshSection(configFile.AppSettings.SectionInformation.Name);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error writing app settings");
+            }
+        }
 
-        //    if (!File.Exists(path))
-        //    {
-        //        //Create the directory
-        //        if (!Directory.Exists(path))
-        //            Directory.CreateDirectory(directory);
-
-        //        //Create the empty file
-        //        FileStream fs = File.Create(path);
-        //    }
-        //}
     }
 }
